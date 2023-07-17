fcc8d54c8da0b616bcd438dd41733f681aa00a12
RedAngelRedux <sammy@sammynava.com>
2023-07-17 15:54:01 -0500

    Add project files.

---
+++ LoanSharkMVC/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using LoanSharkMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoanSharkMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult App()
+        {
+            Loan loan = new()
+            {
+                Payment = 0.00M,
+                TotalInterest = 0.00M,
+                TotalCost = 0.00M,
+                Rate = 3.50M,
+                Amount = 150000.00M,
+                Term = 60                
+            };
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        public IActionResult App(Loan loan)
+        {
+            return View(loan);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
